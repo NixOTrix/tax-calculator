1e25271c69b43f2185cd170f2c4b8b2398bf975b
Nik Mahmutagic <nikmahmutagic@gmail.com>
2023-06-23 07:50:43 +1000

    react work

--- frontEnd/tax-calculator/src/App.tsx
+++ frontEnd/tax-calculator/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+  
+const routeChange = () =>{ 
+  let path = `newPath`; 
+}
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hi and welcome to a simple landing page <br/> for my tax calculator app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={routeChange}>
+          CALCULATE YOUR TAX NOW
+        </button>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
